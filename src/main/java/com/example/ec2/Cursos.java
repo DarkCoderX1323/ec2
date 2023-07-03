77d9d472e06dde376c058e7cde20f699477e5d4c
DarkCoderX1323 <104177440+DarkCoderX1323@users.noreply.github.com>
2023-07-03 14:08:09 +0000

    Fix

--- src/main/java/com/example/ec2/Cursos.java
+++ src/main/java/com/example/ec2/Cursos.java
@@ -1,12 +1,12 @@
 package com.example.ec2;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-public class Curso {
+public class Cursos {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
--- src/main/java/com/example/ec2/IdatController.java
+++ src/main/java/com/example/ec2/IdatController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IDATController {
+public class IdatController {
 
     @GetMapping("/")
     public String mostrarCodigoYNombre() {
