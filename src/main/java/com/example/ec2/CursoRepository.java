0441f3365cc7df32b019670c673a36e33774b6a7
DarkCoderX1323 <104177440+DarkCoderX1323@users.noreply.github.com>
2023-07-03 14:08:34 +0000

    Fix

--- src/main/java/com/example/ec2/CursoRepository.java
+++ src/main/java/com/example/ec2/CursoRepository.java
@@ -2,5 +2,5 @@ package com.example.ec2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CursoRepository extends JpaRepository<Curso, Long> {
+public interface CursoRepository extends JpaRepository<Cursos, Long> {
 }
